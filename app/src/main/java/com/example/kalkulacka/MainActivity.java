f9fe21c3ae884e02599c73ce0a7b1e678c415fdf
lukas-havlicek-krizik <lukas.havlicek@skolakrizik.cz>
2025-02-18 22:50:04 +0100

    Added options for spinner - dropdown menu & little layout changes.

--- app/src/main/java/com/example/kalkulacka/MainActivity.java
+++ app/src/main/java/com/example/kalkulacka/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.kalkulacka;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,18 +11,34 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.lang.reflect.Array;
+
 public class MainActivity extends AppCompatActivity {
+
+    Spinner spinner;
+
+    Button tlacitko;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_main);
+
+        spinner = findViewById(R.id.spinner);
+        tlacitko = findViewById(R.id.tlacitko);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.spinnerVyber, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
     }
 
 
